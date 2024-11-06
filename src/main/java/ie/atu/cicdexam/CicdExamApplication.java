e54fa38b9393bfc8916a0079938060423ad0be97
G00413711@atu.ie <G00413711@atu.ie>
2024-11-06 13:56:13 +0000

    created project

---
+++ src/main/java/ie/atu/cicdexam/CicdExamApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdExamApplication.class, args);
+    }
+
+}
